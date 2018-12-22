c537f5d67e1deed0f5ca917e29264ff3ba5cf7c0
SorinAndreiCristea <sorin_9931@yahoo.com>
2018-12-22 02:13:49 +0200

    Finished assets + animations

---
+++ Unity-Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+        anim.SetFloat("Move", Input.GetAxisRaw("Horizontal"));
+    }
+
+    
+
+}
