a729c950ed9587825fef9e85b5afc5fe6fc543cd
SorinAndreiCristea <sorin_9931@yahoo.com>
2019-01-10 09:04:29 +0200

    Buttons + some scene transitions

---
+++ Unity-Project/Assets/Scripts/SceneSelection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelection : MonoBehaviour
+{
+   public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Instructions ()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void QuitGame ()
+    { 
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
